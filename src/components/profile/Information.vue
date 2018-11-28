be836c119c9fdaa1e00011312e2be3200300bba9
0914yang <43604235+0914yang@users.noreply.github.com>
2018-11-28 23:27:26 +0800

    Update Information.vue

--- src/components/profile/Information.vue
+++ src/components/profile/Information.vue
@@ -1,16 +1,180 @@
 <template>
   <div class="information">
-
+    <el-container>
+  		<el-aside width="200px">
+    		  			<el-row style="height: 50%;" type="flex" justify="left">
+     			<el-col :span="24"><div class="grid-content">
+     				<ul>
+        				<li><a class="active" href="/">个人信息</a></li>
+        				<li><a class="active" href="/">职业意向</a></li>
+     				</ul>
+      			</div></el-col>
+    		</el-row>
+  		</el-aside>
+  		<el-main>
+  			<el-row style="height: 50%;" type="flex" justify="left">
+     			<el-col :span="24"><div class="grid-content">
+        			<h1>基本信息</h1>
+      			</div></el-col>
+    		</el-row>
+    		  <el-row style="height: 50%;" :type="flex" justify="left">
+    		  	<el-col :span="4"><div class="grid-content">
+        			<p>姓名</p>
+      			</div></el-col>
+     			<el-col :span="4"><div class="grid-content">
+        			<el-input size="small" v-model="input1" placeholder=""></el-input>
+      			</div></el-col>
+      			<el-col :span="4"><div class="grid-content">
+        			<p>居住地</p>
+      			</div></el-col>
+      			<el-col :span="2"><div class="grid-content">
+        			<el-input size="small" v-model="input2" placeholder="省份"></el-input>
+      			</div></el-col>
+      			<el-col :span="2"><div class="grid-content">
+        			<el-input size="small" v-model="input3" placeholder="城市"></el-input>
+      			</div></el-col>
+    		</el-row>
+    		</el-row>
+    		  <el-row style="height: 50%;" :type="flex" justify="left">
+    		  	<el-col :span="4"><div class="grid-content">
+        			<p>性别</p>
+      			</div></el-col>
+     			<el-col :span="4"><div class="grid-content">
+        			<el-radio v-model="radio" label="1">男</el-radio>
+  					<el-radio v-model="radio" label="2">女</el-radio>
+      			</div></el-col>
+      			<el-col :span="4"><div class="grid-content">
+        			<p>出生日期</p>
+      			</div></el-col>
+      			<el-col :span="4"><div class="grid-content">
+        			<el-date-picker v-model="value1" type="date" placeholder="选择日期"></el-date-picker>
+      			</div></el-col>
+    		</el-row>
+    	    </el-row>
+    		  <el-row style="height: 50%;" :type="flex" justify="left">
+    		  	<el-col :span="4"><div class="grid-content">
+        			<p>最高学历</p>
+      			</div></el-col>
+     			<el-col :span="4"><div class="grid-content">
+        			  <el-select v-model="value" placeholder="请选择">
+    					<el-option
+      						v-for="item in options"
+      						:key="item.value"
+     						 :label="item.label"
+     						 :value="item.value">
+   						 </el-option>
+					  </el-select>
+      			</div></el-col>
+    		</el-row>
+    		<el-row style="height: 50%;" type="flex" justify="left">
+     			<el-col :span="24"><div class="grid-content">
+        			<h1>联系信息</h1>
+      			</div></el-col>
+    		</el-row>
+    	    </el-row>
+    		  <el-row style="height: 50%;" :type="flex" justify="left">
+    		  	<el-col :span="4"><div class="grid-content">
+        			<p>手机号码</p>
+      			</div></el-col>
+     			<el-col :span="4"><div class="grid-content">
+        			<el-input size="small" v-model="input4" placeholder=""></el-input>
+      			</div></el-col>
+      			<el-col :span="4"><div class="grid-content">
+        			<p>邮箱地址</p>
+      			</div></el-col>
+      			<el-col :span="4"><div class="grid-content">
+        			<el-input size="small" v-model="input5" placeholder=""></el-input>
+      			</div></el-col>
+    		</el-row>
+    		<el-row style="height: 50%;" type="flex" justify="left">
+     			<el-col :span="24"><div class="grid-content">
+        			 <el-button>保存</el-button>
+      			</div></el-col>
+    		</el-row>
+  		</el-main>
+   	</el-container>
   </div>
 </template>
 
 <script>
 
 export default {
-  
+  name:"app",
+  data() {
+        return {
+          input1: '',
+          input2: '',
+          input3: '',
+          input4: '',
+          input5: '',
+          radio: '1',
+          options: [{
+          value: '选项1',
+          label: '高中'
+        }, {
+          value: '选项2',
+          label: '本科'
+        }, {
+          value: '选项3',
+          label: '硕士'
+        }, {
+          value: '选项4',
+          label: '博士'
+        }],
+        value: ''
+        }
+  }  
 };
 </script>
 
 <style scoped>
+.information {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+}
 
+.grid-content {
+
+  height: 100%;
+}
+
+.el-aside {
+  height: 100%;
+  background-color: #D3DCE6;
+  color: #333;
+  line-height: 60px;
+  font-size: 20px;
+}
+  
+.el-main {
+  height: 100%;
+  background-color: #FFFFFF;
+  color: #333;
+  text-align: center top;
+  line-height: 15px;
+  font-weight: bold;
+  font-size: 20px;
+}
+ul {
+	height: 100%;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 200px;
+    background-color: #f1f1f1;
+}
+ 
+li a {
+
+    display: block;
+    color: #000;
+    padding: 8px 16px;
+    text-decoration: none;
+}
+ 
+li a:hover {
+    background-color: #555;
+    color: white;
+}
 </style>
